3a7c60a36c3cb7abf83a37d9a501034b61ddfd9c
Valquer Rafael <valquer.rafael@gmail.com>
2025-11-08 19:44:19 -0300

    complete day 4 of AoC 2015 (poor performance)

---
+++ 2015/day-4/part-1.ts
@@ -0,0 +1,28 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+const crypto = require('crypto');
+
+function generateMd5Hash(input: string): string {
+	return crypto.createHash('md5').update(input).digest('hex');
+}
+
+const rl = readline.createInterface({ input, output });
+let positiveNumber = 1;
+let shouldContinue = true;
+
+rl.question('Input: ', (input: string) => {
+	while (shouldContinue) {
+		const md5Hash = generateMd5Hash(`${input}${positiveNumber}`);
+
+		if (md5Hash.startsWith('00000')) {
+			shouldContinue = false;
+			break;
+		} else {
+			positiveNumber += 1;
+		}
+	}
+
+	console.log(positiveNumber);
+
+	rl.close();
+});
